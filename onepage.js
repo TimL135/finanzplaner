c716a6890f4228a24791e840f55d8cc2369bd48c
tim <t.littschwager@mbd-team.de>
2021-08-23 12:36:45 +0200

    pläneBearbeiten/checkboxenErstellen/style

--- onepage.js
+++ onepage.js
@@ -1,74 +1,104 @@
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
-var btnEingabeEinnahme = document.getElementById("einnahmeBestätigung");
-var btnEingabeAusgabe = document.getElementById("ausgabeBestätigung");
+let btnEingabeEinnahme = document.getElementById("einnahmeBestätigung");
+let btnEingabeAusgabe = document.getElementById("ausgabeBestätigung");
 
-var btnEingabePlan = document.getElementById("planBestätigung");
-var pläneerstellen = document.getElementById("pläneerstellen");
+let btnEingabePlan = document.getElementById("planBestätigung");
+let pläneerstellen = document.getElementById("pläneerstellen");
 
-var carouselAnzahl = 0;
-var objectPlan = {};
-var einnahmeArray = [];
-var ausgabeArray = [];
+let plänebearbeiten = document.getElementById("planEditBestätigung")
 
-var begrußung = document.getElementById("begrußung");
-var div = document.createElement("div");
-var text = ` Guten Tag, User`;
+let carouselAnzahl = 0;
+let objectPlan = {};
+let einnahmeArray = [];
+let ausgabeArray = [];
+
+let checkedCheckbox = -1;
+
+let begrußung = document.getElementById("begrußung");
+let div = document.createElement("div");
+let text = ` Guten Tag, User`;
 div.appendChild(document.createTextNode(text));
 begrußung.appendChild(div);
 
 btnEingabeEinnahme.onclick = function (event) {
-    var eingabeGrund = document.getElementById("einnahmeGrund").value;
-    var eingabeMenge = document.getElementById("einnahmeMenge").value;
+    let einnahmeGrund = document.getElementById("einnahmeGrund").value;
+    let einnahmeMenge = document.getElementById("einnahmeMenge").value;
 
-    einnahmeArray.push(eingabeMenge);
+    if (einnahmeMenge && einnahmeGrund != "") {
+        einnahmeArray.push(einnahmeMenge);
 
-    var einnahmeGrundListe = document.getElementById("einnahmeGrundListe");
-    var einnahmeMengeListe = document.getElementById("einnahmeMengeListe");
-    var einnahmeDatumListe = document.getElementById("einnahmeDatumListe");
+        let einnahmeGrundListe = document.getElementById("einnahmeGrundListe");
+        let einnahmeMengeListe = document.getElementById("einnahmeMengeListe");
+        let einnahmeDatumListe = document.getElementById("einnahmeDatumListe");
 
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(eingabeGrund));
-    einnahmeGrundListe.appendChild(li);
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(einnahmeGrund));
+        einnahmeGrundListe.appendChild(li);
 
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(eingabeMenge));
-    einnahmeMengeListe.appendChild(li);
+        li = document.createElement("li");
+        li.appendChild(document.createTextNode(`${einnahmeMenge}€`));
+        einnahmeMengeListe.appendChild(li);
 
 
-    var heute = new Date();
-    var einnahmeDatum = `${heute.getDate()} ${heute.getMonth() + 1} ${heute.getFullYear()}`;
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(einnahmeDatum));
-    einnahmeDatumListe.appendChild(li);
+        let heute = new Date().toLocaleDateString();
+        li = document.createElement("li");
+        li.appendChild(document.createTextNode(heute));
+        einnahmeDatumListe.appendChild(li);
 
-    gesamtBerechnen();
+        gesamtBerechnen();
 
-    document.getElementById("einnahmeGrund").value = "";
-    document.getElementById("einnahmeMenge").value = "";
+        document.getElementById("einnahmeGrund").value = "";
+        document.getElementById("einnahmeMenge").value = "";
+    }
 }
 btnEingabeAusgabe.onclick = function (event) {
-    var ausgabeGrund = document.getElementById("ausgabeGrund").value;
-    var ausgabeMenge = document.getElementById("ausgabeMenge").value;
+    let ausgabeGrund = document.getElementById("ausgabeGrund").value;
+    let ausgabeMenge = document.getElementById("ausgabeMenge").value;
+    let checkboxNumber = 0;
 
+    if (ausgabeMenge != "") {
+        for (let checkbox of document.querySelectorAll(".checkbox")) {
+            if (document.getElementById(`checkbox${checkboxNumber}`).checked == true) {
+                checkedCheckbox = checkboxNumber;
+            }
+            checkboxNumber++;
+        }
+    }
+    if (checkedCheckbox > -1) {
+        document.getElementById(`checkbox${checkedCheckbox}`).checked = false
+        ausgabeFunction(`Plan${checkedCheckbox + 1}`, ausgabeMenge);
+    } else {
+        if (ausgabeMenge && ausgabeGrund != "") {
+            ausgabeFunction(ausgabeGrund, ausgabeMenge);
+        }
+    }
+}
+
+function ausgabeFunction(ausgabeGrund, ausgabeMenge) {
+    if (checkedCheckbox > -1) {
+        let planid = document.getElementById(`plan${checkedCheckbox}`)
+        planChange(objectPlan[checkedCheckbox].planNummer, planid, objectPlan[checkedCheckbox].planGrund, objectPlan[checkedCheckbox].planDauer, objectPlan[checkedCheckbox].planMenge - ausgabeMenge);
+        objectPlan[checkedCheckbox].planMenge -= ausgabeMenge;
+    }
+    checkedCheckbox = -1
     ausgabeArray.push(ausgabeMenge);
 
-    var ausgabeGrundListe = document.getElementById("ausgabeGrundListe");
-    var ausgabeMengeListe = document.getElementById("ausgabeMengeListe");
-    var ausgabeDatumListe = document.getElementById("ausgabeDatumListe");
+    let ausgabeGrundListe = document.getElementById("ausgabeGrundListe");
+    let ausgabeMengeListe = document.getElementById("ausgabeMengeListe");
+    let ausgabeDatumListe = document.getElementById("ausgabeDatumListe");
 
-    var li = document.createElement("li");
+    let li = document.createElement("li");
     li.appendChild(document.createTextNode(ausgabeGrund));
     ausgabeGrundListe.appendChild(li);
 
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(ausgabeMenge));
+    li = document.createElement("li");
+    li.appendChild(document.createTextNode(`${ausgabeMenge}€`));
     ausgabeMengeListe.appendChild(li);
 
-    var heute = new Date();
-    var ausgabeDatum = `${heute.getDate()} ${heute.getMonth() + 1} ${heute.getFullYear()}`;
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(ausgabeDatum));
+    let heute = new Date().toLocaleDateString();
+    li = document.createElement("li");
+    li.appendChild(document.createTextNode(heute));
     ausgabeDatumListe.appendChild(li);
 
     gesamtBerechnen();
@@ -79,83 +109,173 @@ btnEingabeAusgabe.onclick = function (event) {
 }
 
 btnEingabePlan.onclick = function (event) {
-    var planCarousel = document.getElementById("planCarousel");
-    var carouselItemFirst = document.getElementById("carouselItemFirst")
-    if (carouselAnzahl == 0) {
-        var planGrund = document.getElementById("planGrund").value;
-        var planMenge = document.getElementById("planMenge").value;
-        var planDauer = document.getElementById("planDauer").value;
-        objectPlan[carouselAnzahl] = {
-            planGrund: planGrund,
-            planMenge: planMenge,
-            planDauer: planDauer
-        }
-        var plan = [planGrund, planMenge, planDauer];
-        var div = document.createElement("div");
-        carouselItemFirst.removeChild(carouselItemFirst.firstChild);
-        carouselItemFirst.removeChild(carouselItemFirst.firstChild);
-        carouselItemFirst.removeChild(carouselItemFirst.firstChild);
-        carouselItemFirst.appendChild(document.createTextNode(`Plan ${carouselAnzahl + 1}`));
-        for (let i = 0; i < 3; i++) {
-            var br = document.createElement("br");
+    let planCarousel = document.getElementById("planCarousel");
+    let carouselItemFirst = document.getElementById("carouselItemFirst")
+
+    let planGrund = document.getElementById("planGrund").value;
+    let planMenge = document.getElementById("planMenge").value;
+    let planDauer = document.getElementById("planDauer").value;
+    if (planGrund && planMenge && planDauer != "") {
+        if (carouselAnzahl == 0) {
+            document.getElementById("planBearbeitenKnopf").classList.remove("d-none")
+            objectPlan[carouselAnzahl] = {
+                planGrund: planGrund,
+                planMenge: planMenge,
+                planDauer: planDauer,
+                planNummer: carouselAnzahl
+            }
+            let plan = [planGrund, planMenge, planDauer];
+            let div = document.createElement("div");
+            for (let i = 0; i < 3; i++) {
+
+                carouselItemFirst.removeChild(carouselItemFirst.firstChild);
+            }
+            carouselItemFirst.appendChild(document.createTextNode(`Plan ${carouselAnzahl + 1}`));
+            for (let i = 0; i < 3; i++) {
+                let br = document.createElement("br");
+                carouselItemFirst.appendChild(br)
+                switch (i) {
+                    case 0: carouselItemFirst.appendChild(document.createTextNode(`Du sparst für: ${plan[i]}`)); break
+                    case 1: carouselItemFirst.appendChild(document.createTextNode(`Dir fehlen: ${plan[i]} €`)); break
+                    case 2: carouselItemFirst.appendChild(document.createTextNode(`Du sparst: ${plan[i]} Tage`)); break
+
+
+                }
+            }
+            let br = document.createElement("br");
             carouselItemFirst.appendChild(br)
-            switch (i) {
-                case 0: carouselItemFirst.appendChild(document.createTextNode(`Du sparst für: ${plan[i]}`)); break
-                case 1: carouselItemFirst.appendChild(document.createTextNode(`Du sparst: ${plan[i]}€`)); break
-                case 2: carouselItemFirst.appendChild(document.createTextNode(`Du sparst: ${plan[i]} Tage`)); break
-            }
+            carouselItemFirst.appendChild(document.createTextNode(`Empfehlung: ${Math.round((plan[1] / plan[2]) * 100) / 100}€ pro Tag`));
+            carouselItemFirst.id = `plan${carouselAnzahl}`;
+            planCarousel.appendChild(div);
+            document.getElementById("planGrund").value = "";
+            document.getElementById("planMenge").value = "";
+            document.getElementById("planDauer").value = "";
+            checkboxErstellen(planGrund, carouselAnzahl);
+            carouselAnzahl++;
+        } else {
 
-        }
-        carouselItemFirst.id = `plan${carouselAnzahl}`;
-        carouselAnzahl++;
-        planCarousel.appendChild(div);
-        document.getElementById("planGrund").value = "";
-        document.getElementById("planMenge").value = "";
-        document.getElementById("planDauer").value = "";
-    } else {
-        var planGrund = document.getElementById("planGrund").value;
-        var planMenge = document.getElementById("planMenge").value;
-        var planDauer = document.getElementById("planDauer").value;
-        objectPlan[carouselAnzahl] = {
-            planGrund: planGrund,
-            planMenge: planMenge,
-            planDauer: planDauer
-        }
-        var plan = [planGrund, planMenge, planDauer];
-        var div = document.createElement("div");
-        div.appendChild(document.createTextNode(`Plan ${carouselAnzahl + 1}`));
-        for (let i = 0; i < 3; i++) {
-            var br = document.createElement("br");
+            objectPlan[carouselAnzahl] = {
+                planGrund: planGrund,
+                planMenge: planMenge,
+                planDauer: planDauer,
+                planNummer: carouselAnzahl
+            }
+            let plan = [planGrund, planMenge, planDauer];
+            let div = document.createElement("div");
+            div.appendChild(document.createTextNode(`Plan ${carouselAnzahl + 1}`));
+            for (let i = 0; i < 3; i++) {
+                let br = document.createElement("br");
+                div.appendChild(br)
+                switch (i) {
+                    case 0: div.appendChild(document.createTextNode(`Du sparst für: ${plan[i]}`)); break
+                    case 1: div.appendChild(document.createTextNode(`Dir fehlen: ${plan[i]} €`)); break
+                    case 2: div.appendChild(document.createTextNode(`Du sparst: ${plan[i]} Tage`)); break
+                }
+            }
+            for (let carouselItem of document.querySelectorAll(".carousel-item")) {
+                carouselItem.classList.remove("active")
+            }
+            let br = document.createElement("br");
             div.appendChild(br)
-            switch (i) {
-                case 0: div.appendChild(document.createTextNode(`Du sparst für: ${plan[i]}`)); break
-                case 1: div.appendChild(document.createTextNode(`Du sparst: ${plan[i]}€`)); break
-                case 2: div.appendChild(document.createTextNode(`Du sparst bis: ${plan[i]} Tage`)); break
-            }
-
+            div.appendChild(document.createTextNode(`Empfehlung: ${Math.round((plan[1] / plan[2]) * 100) / 100}€ pro Tag`));
+            div.classList.add("carousel-item", "active");
+            div.id = `plan${carouselAnzahl}`;
+            planCarousel.appendChild(div);
+            document.getElementById("planGrund").value = "";
+            document.getElementById("planMenge").value = "";
+            document.getElementById("planDauer").value = "";
+            checkboxErstellen(planGrund, carouselAnzahl);
+            carouselAnzahl++;
         }
-        div.classList.add("carousel-item");
-        div.id = `plan${carouselAnzahl}`;
-        carouselAnzahl++;
-        planCarousel.appendChild(div);
-        document.getElementById("planGrund").value = "";
-        document.getElementById("planMenge").value = "";
-        document.getElementById("planDauer").value = "";
     }
 }
+function checkboxErstellen(planGrund, planNummer) {
+    ul = document.getElementById("ausgabePlan");
+    let li = document.createElement("li");
+    let checkbox = document.createElement("INPUT");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("name", "check");
+    checkbox.setAttribute("onclick", "onlyOne(this)");
+    checkbox.classList.add("checkbox");
+    checkbox.id = `checkbox${planNummer}`
+    let label = document.createElement("Label");
+    label.innerHTML = `Sparen für: ${planGrund}`
+    label.setAttribute("for", `checkbox${planNummer}`);
+    label.setAttribute("style", "margin-left:5px")
+    li.appendChild(checkbox);
+    li.appendChild(label)
+    ul.appendChild(li);
+}
 function gesamtBerechnen() {
-    var gesamt = 0;
+    let gesamt = 0;
     for (let i = 0; i < einnahmeArray.length; i++) {
         gesamt += parseInt(einnahmeArray[i]);
     }
     for (let i = 0; i < ausgabeArray.length; i++) {
         gesamt -= parseInt(ausgabeArray[i]);
     }
-    var gesamtText = document.getElementById("gesamt");
+    let gesamtText = document.getElementById("gesamt");
     gesamtText.removeChild(gesamtText.firstChild);
-    var div = document.createElement("div");
+    let div = document.createElement("div");
     div.appendChild(document.createTextNode(`${gesamt}€`));
+    if (gesamt > 0) {
+        gesamtText.setAttribute("style", "color:green")
+    }
+    if (gesamt < 0) {
+        gesamtText.setAttribute("style", "color:red")
+    }
+    if (gesamt == 0) {
+        gesamtText.setAttribute("style", "color:#212529")
+    }
     gesamtText.appendChild(div);
     return gesamt;
 }
+plänebearbeiten.onclick = function (event) {
+    let planNummer = document.getElementById("planEditNummer").value
+    let planGrund = document.getElementById("planEditGrund").value
+    let planDauer = document.getElementById("planEditDauer").value
+    let planMenge = document.getElementById("planEditMenge").value
 
+    if (planNummer && planGrund != "") {
+        if (planDauer && planMenge != "") {
+            planNummer--
+            let planid = document.getElementById(`plan${planNummer}`)
+            document.getElementById("planEditGrund").value = ""
+            document.getElementById("planEditDauer").value = ""
+            document.getElementById("planEditMenge").value = ""
+            document.getElementById("planEditNummer").value = ""
+
+            planChange(planNummer, planid, planGrund, planDauer, planMenge)
+        }
+    }
+
+}
+function planChange(planNummer, planid, planGrund, planDauer, planMenge) {
+    let plan = [planGrund, planMenge, planDauer];
+    for (let i = 0; i < 9; i++) {
+        planid.removeChild(planid.firstChild);
+    }
+    planid.appendChild(document.createTextNode(`Plan ${objectPlan[planNummer].planNummer + 1}`));
+    for (let i = 0; i < 3; i++) {
+        let br = document.createElement("br");
+        planid.appendChild(br)
+        switch (i) {
+            case 0: planid.appendChild(document.createTextNode(`Du sparst für: ${plan[i]}`)); break
+            case 1: planid.appendChild(document.createTextNode(`Dir fehlen: ${plan[i]} €`)); break
+            case 2: planid.appendChild(document.createTextNode(`Du sparst: ${plan[i]} Tage`)); break
+
+        }
+    }
+    let br = document.createElement("br");
+    planid.appendChild(br)
+    planid.appendChild(document.createTextNode(`Empfehlung: ${Math.round((plan[1] / plan[2]) * 100) / 100}€ pro Tag`));
+}
+function onlyOne(checkbox) {
+    var checkboxes = document.getElementsByName('check')
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
+function planLöschen() {
+
+}
